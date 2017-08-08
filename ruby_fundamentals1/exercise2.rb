6952c0685d0c4a8276972e988a2539f10e64c0fb
Matt Paoli <mattpaoli@hotmail.com>
2017-08-08 16:08:35 -0400

    Upload exercise 2 & start of 3

---
+++ ruby_fundamentals1/exercise2.rb
@@ -0,0 +1,9 @@
+puts 'How would you calculate a good tip for a 55 dollar meal?'#Question
+puts "55 * 1.5" #Equation
+puts 55 * 1.5 #Answer
+
+puts "The tip I want to calculate is #{55+50}" #A string in addition to an equation using the (+) operator
+
+puts "The value I wish to calculate is 45628 multiplied by 7839 which will give me a total of #{45628+7839}" #Using string interpolation
+
+puts (10 < 20 && 30 < 20) || !(10 == 11) #The value that is expressed is ten is less than twenty and thirty is less than twenty OR  ten is not equal to eleven
